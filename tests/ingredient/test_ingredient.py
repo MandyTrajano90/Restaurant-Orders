419aaba695cf19cdbb0af46809bd86dbed6f1f18
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-17 22:22:45 -0300

    First Req - tests of ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
-#Starting
+
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("salsinha")
+    ingredient2 = Ingredient("salsinha")
+
+    assert ingredient1.name == "salsinha"
+    assert ingredient1.restrictions == set()
+
+    assert hash(ingredient1) == hash("salsinha")
+
+    assert ingredient1 == ingredient2
+
+    assert repr(ingredient1) == "Ingredient('salsinha')"
