795b546af2bfe133febf41fc1cb284cd294086f5
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-17 23:06:19 -0300

    Third Req - feat class MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,31 @@
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.menu(source_path)
+
+    def menu(self, source_path: str) -> None:
+        with open(source_path, mode='r', newline='') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
+            for row in csv_reader:
+                dish_name, price, ingredient_name, quantity = row
+                ct_dish = self.get_new_dish(dish_name)
+
+                if ct_dish is None:
+                    ct_dish = Dish(dish_name, float(price))
+                    self.dishes.add(ct_dish)
+
+                ingredient = Ingredient(ingredient_name)
+                ct_dish.add_ingredient_dependency(ingredient, int(quantity))
+
+    def get_new_dish(self, dish_name: str):
+        for new_dish in self.dishes:
+            if new_dish.name == dish_name:
+                return new_dish
+        return None
